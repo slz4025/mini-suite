5e3e469cd27c43fedf89c519ba949c4dda4f5cb5
slz4025 <slzheng@alumni.cmu.edu>
2024-05-09 04:05:42 +0000

    move editing func

--- spreadsheet/static/editor.js
+++ spreadsheet/static/editor.js
@@ -1,3 +1,19 @@
+function editing() {
+  const activeId = document.activeElement.id;
+  if (activeId == "editor-contents") {
+    return true;
+  }
+  else if (activeId == "value-contents") {
+    return true;
+  }
+  else if (activeId.startsWith("input-cell-")) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 function update_editor(row, col) {
     htmx.ajax('PUT', `/cell/${row}/${col}/focus`, {
       target: "#editor",

7a114ba48927ca4f2681d116db35a9dac7d0b9e9
slz4025 <slzheng@alumni.cmu.edu>
2024-05-25 20:52:51 -0700

    remap keys; no enter

--- spreadsheet/static/key_bindings.js
+++ spreadsheet/static/key_bindings.js
@@ -1,128 +1,97 @@
 window.addEventListener('keydown', function(event) {
-  if (editing()) {
-    const focused_cell = get_focused_cell();
-    // if editing is true, focused_cell must not be undefined
-    const [row, col] = focused_cell;
+  if (event.ctrlKey) {
+    switch (event.key) {
+      // modes
+      case '0':
+        document.getElementById("help-toggler").click();
+        break;
+      case '1':
+        document.getElementById("editor-toggler").click();
+        break;
+      case '2':
+        document.getElementById("selection-toggler").click();
+        break;
+      case '3':
+        document.getElementById("bulk-editor-toggler").click();
+        break;
+      case '4':
+        document.getElementById("navigator-toggler").click();
+        break;
 
-    // The native behavior is that
-    // Tab goes right or next
-    // and Shift-Tab goes left or previous.
-    // We therefore only encode the desired behavior
-    // for Enter (down) and Shift-Enter (up).
-    let nextId;
-    if (event.shiftKey) {
-      switch (event.key) {
-        case 'Enter':
-          event.preventDefault();
-          nextId = `cell-${row-1}-${col}`;
-          break;
-      }
-    } else {
-      switch (event.key) {
-        case 'Enter':
-          event.preventDefault();
-          nextId = `cell-${row+1}-${col}`;
-          break;
-      }
-    }
+      // selection
+      case 'w':
+        document.getElementById("up-selection-button")?.click();
+        break;
+      case 's':
+        document.getElementById("down-selection-button")?.click();
+        break;
+      case 'a':
+        document.getElementById("left-selection-button")?.click();
+        break;
+      case 'd':
+        document.getElementById("right-selection-button")?.click();
+        break;
 
-    const element = document.getElementById(nextId);
-    if (element) {
-      element.focus();
-    }
-  } else {
-    if (event.ctrlKey) {
-      switch (event.key) {
-        // modes
-        case '0':
-          document.getElementById("help-toggler").click();
-          break;
-        case '1':
-          document.getElementById("editor-toggler").click();
-          break;
-        case '2':
-          document.getElementById("selection-toggler").click();
-          break;
-        case '3':
-          document.getElementById("bulk-editor-toggler").click();
-          break;
-        case '4':
-          document.getElementById("navigator-toggler").click();
-          break;
-
-        // bulk edit
-        case 'x':
+      // bulk edit
+      case 'x':
+        if (!editing()) {
           htmx.ajax('POST', `/bulk-editor/apply/Cut`, {
             target: `#bulk-editor`,
             swap: "outerHTML",
           });
-          break;
-        case 'c':
+        }
+        break;
+      case 'c':
+        if (!editing()) {
           htmx.ajax('POST', `/bulk-editor/apply/Copy`, {
             target: `#bulk-editor`,
             swap: "outerHTML",
           });
-          break;
-        case 'v':
+        }
+        break;
+      case 'v':
+        if (!editing()) {
           htmx.ajax('POST', `/bulk-editor/apply/Paste`, {
             target: `#bulk-editor`,
             swap: "outerHTML",
           });
-          break;
+        }
+        break;
 
-        // view
-        case 'h':
-          document.getElementById("home-button").click();
-          break;
-        case 't':
-          document.getElementById("navigator-target-button").click();
-          break;
+      // view
+      case 'h':
+        document.getElementById("home-button").click();
+        break;
+      case 't':
+        document.getElementById("navigator-target-button").click();
+        break;
+      case 'i':
+        document.getElementById("up-button").click();
+        break;
+      case 'k':
+        document.getElementById("down-button").click();
+        break;
+      case 'j':
+        document.getElementById("left-button").click();
+        break;
+      case 'l':
+        document.getElementById("right-button").click();
+        break;
 
-        // save
-        case 's':
-          document.getElementById("files-save").click();
-          break;
-      }
-    } else if (event.shiftKey) {
-      switch (event.key) {
-        // view
-        case 'ArrowUp':
-          document.getElementById("up-button").click();
-          break;
-        case 'ArrowDown':
-          document.getElementById("down-button").click();
-          break;
-        case 'ArrowLeft':
-          document.getElementById("left-button").click();
-          break;
-        case 'ArrowRight':
-          document.getElementById("right-button").click();
-          break;
-
-        // selection
-        case 'W':
-          document.getElementById("up-selection-button")?.click();
-          break;
-        case 'S':
-          document.getElementById("down-selection-button")?.click();
-          break;
-        case 'A':
-          document.getElementById("left-selection-button")?.click();
-          break;
-        case 'D':
-          document.getElementById("right-selection-button")?.click();
-          break;
-      }
-    } else {
-      switch (event.key) {
-        // On a Mac, this is done via Fn+Backspace (which might say "delete").
-        case 'Delete':
-          htmx.ajax('POST', `/bulk-editor/apply/Delete`, {
-            target: `#bulk-editor`,
-            swap: "outerHTML",
-          });
-          break;
-      }
+      // save
+      case 's':
+        document.getElementById("files-save").click();
+        break;
+    }
+  } else {
+    switch (event.key) {
+      // On a Mac, this is done via Fn+Backspace (which might say "delete").
+      case 'Delete':
+        htmx.ajax('POST', `/bulk-editor/apply/Delete`, {
+          target: `#bulk-editor`,
+          swap: "outerHTML",
+        });
+        break;
     }
   }
 });

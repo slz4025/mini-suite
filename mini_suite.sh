58613f9a90e264fac1fce336003d41d5671c1a24
slz4025 <slzheng@alumni.cmu.edu>
2024-06-09 23:26:21 +0000

    static port

--- mini_suite.sh
+++ mini_suite.sh
@@ -3,13 +3,13 @@
 sheet() {
     sheet_dir="${MINISUITE_PATH}/spreadsheet"
     source "${sheet_dir}/.venv/bin/activate"
-    python "${sheet_dir}/spreadsheet.py" "$@"
+    python "${sheet_dir}/spreadsheet.py" --port 5000 "$@"
 }
 
 doc() {
     doc_dir="${MINISUITE_PATH}/document"
     source "${doc_dir}/.venv/bin/activate"
-    python "${doc_dir}/document.py" file "$@"
+    python "${doc_dir}/document.py" --port 5000 file "$@"
 }
 
 wiki() {

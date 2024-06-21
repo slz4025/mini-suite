c6b15ba3b0d3b839a1bcf466dfc2d01da8996601
slz4025 <slzheng@alumni.cmu.edu>
2024-06-21 14:22:05 +0000

    more keys

--- document/static/key_bindings.js
+++ document/static/key_bindings.js
@@ -4,26 +4,29 @@ function inputting() {
     return ["INPUT", "TEXTAREA"].includes(nodeType);
 }
 
-window.addEventListener('keyup', async function(event) {
-  if (event.shiftKey) {
-    switch (event.key) {
-      case 'ArrowUp':
-        document.getElementById("prev-block-button")?.click();
-        break;
-      case 'ArrowDown':
-        document.getElementById("next-block-button")?.click();
-        break;
-    }
-  } else {
-    switch (event.key) {
-      case 's':
-        if (!inputting()) {
-          document.getElementById("save").click();
-        }
-        break;
-      case 'Escape':
-        document.getElementById("render-block-button")?.click();
-        break;
-    }
+window.addEventListener('keyup', function(event) {
+  switch (event.key) {
+    /* TODO: Have these also move towards block of interest.
+    case 'PageUp':
+      document.getElementById("prev-block-button")?.click();
+      break;
+    case 'PageDown':
+      document.getElementById("next-block-button")?.click();
+      break;
+    */
+    case 's':
+      if (!inputting()) {
+        document.getElementById("save").click();
+      }
+      break;
+    case 'Escape':
+      document.getElementById("render-block-button")?.click();
+      break;
+    case 'Insert':
+      document.getElementById("insert-block-button")?.click();
+      break;
+    case 'Delete':
+      document.getElementById("delete-block-button")?.click();
+      break;
   }
 });

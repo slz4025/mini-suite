ffe2e1b052a71bf0387fb153217c4adfd71a0b98
slz4025 <slzheng@alumni.cmu.edu>
2024-06-22 20:26:52 +0000

    prevent default

--- document/static/key_bindings.js
+++ document/static/key_bindings.js
@@ -6,24 +6,29 @@ function inputting() {
 
 window.addEventListener('keyup', function(event) {
   switch (event.key) {
-    case 'PageUp':
-      document.getElementById("prev-block-button")?.click();
-      break;
-    case 'PageDown':
-      document.getElementById("next-block-button")?.click();
-      break;
     case 's':
       if (!inputting()) {
         document.getElementById("save").click();
       }
       break;
     case 'Escape':
+      event.preventDefault();
       document.getElementById("render-block-button")?.click();
       break;
+    case 'PageUp':
+      event.preventDefault(); // don't scroll in textarea
+      document.getElementById("prev-block-button")?.click();
+      break;
+    case 'PageDown':
+      event.preventDefault(); // don't scroll in textarea
+      document.getElementById("next-block-button")?.click();
+      break;
     case 'Insert':
+      event.preventDefault();
       document.getElementById("insert-block-button")?.click();
       break;
     case 'Delete':
+      event.preventDefault();
       document.getElementById("delete-block-button")?.click();
       break;
   }
